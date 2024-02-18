2ba39078aba920a25e5531063e3f31e304eb522b
Hexx333 <116751160+Hexx333@users.noreply.github.com>
2024-02-18 17:58:10 +0100

    Add files via upload

---
+++ skryptplan.js
@@ -0,0 +1,19 @@
+document.getElementById("demo").innerHTML = "Hello Dolly.";
+let sender = "acknowdlege@example.com";
+let consumer = "acknowdlege@test.com"
+let emailtitle = "email #1";
+let emailContent = "Hello User!";
+let emailDate = "20xx";
+let emailAttachment1 = "pliktekstowy";
+let emailAttachment2 = "pliktekstowy";
+console.log(sender).
+document.getElementById(Math.random()*101)
+function let document.DOCUMENT_POSITION_DISCONNECTED
+getSelection; {
+    let emailDate;
+    let emailContent;
+}
+postMessage;
+if (9<1>1)  {
+    'false'
+}
